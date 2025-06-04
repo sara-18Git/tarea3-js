80f6c3851148b0424b5745227bc221c667574b76
Sara Robles <saraabigailrobles9@gmail.com>
2025-06-03 21:24:57 -0300

    COMMIT CON AVANCES DE LOS EJERCICIOS

---
+++ js/ejrcicio3.js
@@ -0,0 +1,12 @@
+let opciones = parseInt(
+  prompt(
+    "Menú de cajero automático\n" +
+      "1-Consultar saldo\n" +
+      "2-Retirar dineros\n" +
+      "3-Depositar dinero\n"+
+      "4-Ver últimos movimientos\n"+
+      "5-Cambiar clave:\n"+
+      "6-Transferir dinero:\n"+
+      "7-salir:\n"
+  )
+);
