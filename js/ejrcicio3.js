08b78c826564f57e63d3048dab4ce128093e04d2
sara-18Git <saraabigailrobles9@gmail.com>
2025-06-03 21:57:35 -0300

    avances del ejercico 3

--- js/ejrcicio3.js
+++ js/ejrcicio3.js
@@ -3,10 +3,37 @@ let opciones = parseInt(
     "Menú de cajero automático\n" +
       "1-Consultar saldo\n" +
       "2-Retirar dineros\n" +
-      "3-Depositar dinero\n"+
-      "4-Ver últimos movimientos\n"+
-      "5-Cambiar clave:\n"+
-      "6-Transferir dinero:\n"+
+      "3-Depositar dinero\n" +
+      "4-Ver últimos movimientos\n" +
+      "5-Cambiar clave:\n" +
+      "6-Transferir dinero:\n" +
       "7-salir:\n"
   )
 );
+let opcion = "";
+switch (opciones) {
+  case 1:
+    opcion = "Mostrando saldo disponible...";
+    break;
+  case 2:
+    opcion = "Iniciando proceso de retiro...";
+    break;
+  case 3:
+    opcion = "Depósito en curso...";
+    break;
+  case 4:
+    opcion = "Depósito en curso...";
+    break;
+  case 1:
+    opcion = "";
+    break;
+  case 1:
+    opcion = "";
+    break;
+  case 1:
+    opcion = "";
+    break;
+  case 1:
+    opcion = "";
+    break;
+}
